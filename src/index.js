5bf1040c8d0ca06f3cf6c0db8ca5a24d4aed5ccc
Dung Nguyen <dunggoban@gmail.com>
2022-10-29 11:39:08 +0700

    test

--- src/index.js
+++ src/index.js
@@ -51,3 +51,4 @@ ipcMain.on("submitForm", () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+console.log("Hello from")
