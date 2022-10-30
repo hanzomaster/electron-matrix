05161078d1e05f6e7a1ac7c470df483ce26f2237
Thinh Tran <thinhrom11@gmail.com>
2022-10-30 10:29:34 +0700

    using mathjs package for calculation

--- src/renderer.js
+++ src/renderer.js
@@ -1,8 +1,8 @@
 const { ipcRenderer } = require("electron");
+const math = require("mathjs");
 
 document.querySelector("form")?.addEventListener("submit", (e) => {
   e.preventDefault();
-  const input = document.querySelector("input");
 
   const values = document.querySelectorAll("input[type='text']").values();
   let matrix1 = [];
@@ -22,72 +22,29 @@ document.querySelector("form")?.addEventListener("submit", (e) => {
       row = [];
     }
   }
+  matrix1 = math.matrix(matrix1);
+  matrix2 = math.matrix(matrix2);
   const determinant1 = document.querySelector("#determinant1");
   const determinant2 = document.querySelector("#determinant2");
   determinant1.style.display = "inline-block";
   determinant2.style.display = "inline-block";
-  determinant1.textContent = determinant(matrix1);
-  determinant2.textContent = determinant(matrix2);
+  determinant1.textContent = math.abs(math.det(matrix1));
+  determinant2.textContent = math.abs(math.det(matrix2));
 
-  const product = multiplyMatrices(matrix1, matrix2);
+  const product = math.multiply(matrix1, matrix2);
 
   const productResult = document.querySelector(".result");
-  // loop through the product matrix and change the value of input fields
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       productResult.children[0].children[i].children[j].children[0].value =
-        product[i][j];
+        product._data[i][j];
     }
   }
 
-  console.log(multiplyMatrices(matrix1, matrix2));
+  console.log(product);
   ipcRenderer.send("form-submitted", 1);
 });
 
-// NOTE - calculate determinant of a matrix
-function determinant(m) {
-  if (m.length === 0) return 0;
-  if (m.length === 1) return m[0][0];
-  if (m.length === 2) return m[0][0] * m[1][1] - m[0][1] * m[1][0];
-  if (m.length > 2) {
-    const result = m.reduce((prev, curr, i, arr) => {
-      let miniArr = arr
-        .slice(0, i)
-        .concat(arr.slice(i + 1))
-        .map((item) => item.slice(1));
-      return prev + (i % 2 === 0 ? 1 : -1) * curr[0] * determinant(miniArr);
-    }, 0);
-    return Math.abs(result);
-  }
-}
-
-// NOTE - multiply two matrices
-const multiplyMatrices = (m1, m2) => {
-  if (!Array.isArray(m1) || !Array.isArray(m2) || !m1.length || !m2.length) {
-    throw new Error("arguments should be in 2-dimensional array format");
-  }
-  let x = m1.length,
-    z = m1[0].length,
-    y = m2[0].length;
-  if (m2.length !== z) {
-    throw new Error(
-      "number of columns in first matrix should be equal to number of rows in second matrix"
-    );
-  }
-  let productRow = Array.apply(null, new Array(y)).map(
-    Number.prototype.valueOf,
-    0
-  );
-  let product = new Array(x);
-  for (let p = 0; p < x; p++) {
-    product[p] = productRow.slice();
-  }
-  for (let i = 0; i < x; i++) {
-    for (let j = 0; j < y; j++) {
-      for (let k = 0; k < z; k++) {
-        product[i][j] += m1[i][k] * m2[k][j];
-      }
-    }
-  }
-  return product;
+const inverse = (m) => {
+  return math.inv(m);
 };
